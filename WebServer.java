5fa6c47797dc973e0e4bd473e9e33bf09546b2aa
unknown <rsbarhey@ZONE20-WB.cs.ucalgary.ca>
2015-10-22 11:21:54 -0600

    Initial commit Added Tester.java provided by the prof, and a sekeleton code for WebServer.java as suggested by the prof

---
+++ WebServer.java
@@ -0,0 +1,18 @@
+
+public class WebServer {
+
+	public WebServer(int serverPort) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void start() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void stop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
